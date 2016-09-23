acd3d545a98bbbf9eb08e9537a15f2a1a5a031eb
nagoya1997 <fnagoya_hr2@hotmail.com>
2016-09-23 18:57:37 -0300

    linha 8 até 12
    Implementação da exceção

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,11 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+  private String msg;
+  public SaldoInsuficienteException(String msg){
+      super(msg);
+      this.msg = msg;
+    } 
+  }
+  
 }
