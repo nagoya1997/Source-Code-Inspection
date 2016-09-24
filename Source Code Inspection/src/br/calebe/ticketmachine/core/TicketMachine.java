333334c61b210dbf6558a63f3a1753d29497780a
nagoya1997 <fnagoya_hr2@hotmail.com>
2016-09-23 22:18:41 -0300

    linhas 44 até 46
    Remoção de linhas de código errado que não havia lógica de estar lá

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -41,9 +41,9 @@ public class TicketMachine {
     }
 
     public String imprimir() throws SaldoInsuficienteException {
-        if (saldo < valor) {
-            throw new SaldoInsuficienteException();
-        }
+        
+        
+        
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
