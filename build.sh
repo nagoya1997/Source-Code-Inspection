fd84143e914c17c6a6ebab88d4d4fe2e75170396
nagoya1997 <fnagoya_hr2@hotmail.com>
2016-09-28 09:02:35 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "CLASSPATH:"$CLASSPATH
 echo " "
 echo "Compiling these projects:"
 for i in * ; do
-  if [ -d "$i" ] && [ ! "$i" = "lib" ] && [ ! "$i" = "glassfish3" ]&&[ ! "$i" = "pictures" ]; then
+  if [ -d "$i" ] && [ ! "$i" = "lib" ] && [ ! "$i" = "glassfish3" ] && [ ! "$i" = "pictures" ]; then
     cd "$i"
 
     if [ -f "manifest.mf" ]; then
