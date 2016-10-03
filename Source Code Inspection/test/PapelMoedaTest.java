4400e575e4a384a443d63cf7d294d6adb4f71484
NAGOYA <NAGOYA@DESKTOP-IO66NH7>
2016-10-03 16:34:32 -0300

    tentando arrumar #2

--- Source Code Inspection/test/PapelMoedaTest.java
+++ Source Code Inspection/test/PapelMoedaTest.java
@@ -5,19 +5,22 @@
  */
 
 import br.calebe.ticketmachine.core.PapelMoeda;
+import static junit.framework.Assert.assertEquals;
+/*
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+*/
+import org.junit.Test;
 
 /**
  *
  * @author 31548751
  */
 public class PapelMoedaTest {
-    
+    /*
     public PapelMoedaTest() {
     }
     
@@ -36,6 +39,7 @@ public class PapelMoedaTest {
     @After
     public void tearDown() {
     }
+    */
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
