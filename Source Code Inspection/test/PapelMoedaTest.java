73142001de6a8b24edd9305a571ae166d1cf7f02
NAGOYA <NAGOYA@DESKTOP-IO66NH7>
2016-10-03 17:13:22 -0300

    tentando arrumar #3

--- Source Code Inspection/test/PapelMoedaTest.java
+++ Source Code Inspection/test/PapelMoedaTest.java
@@ -1,51 +1,21 @@
+
+import br.calebe.ticketmachine.core.PapelMoeda;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import br.calebe.ticketmachine.core.PapelMoeda;
-import static junit.framework.Assert.assertEquals;
-/*
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-*/
-import org.junit.Test;
+
 
 /**
  *
  * @author 31548751
  */
 public class PapelMoedaTest {
-    /*
-    public PapelMoedaTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    */
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
     
     @Test
     public void getValorTest(){
